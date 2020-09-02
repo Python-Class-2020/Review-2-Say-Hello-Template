899d39ae6447d20188190be025fc8524076f347d
akshatadsule <akshat.adsule@gmail.com>
2020-09-02 09:59:03 -0700

    Add starter code

---
+++ main.py
@@ -0,0 +1,14 @@
+"""
+Given a list of names, say hello to everyone.
+
+Example:
+	input -> ["Steve", "Mark", "Adam"]
+	output -> ["Hello Steve", "Hello Mark", "Hello Adam"]
+"""
+
+def say_hello(list_of_names):
+	#write code here, make sure you return a list
+
+if __name__ == "__main__":
+	#test code here
+	print(say_hello(["Steve", "Mark", "Adam"]))
