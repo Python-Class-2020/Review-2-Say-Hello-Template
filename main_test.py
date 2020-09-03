6aafe8d71cfa0349806dc9aa66ff2e1c3255dc94
Akshat Adsule <akshat.adsule@gmail.com>
2020-09-02 22:53:54 -0700

    fix issue in which pytest would not find tests

--- main_test.py
+++ main_test.py
@@ -1,5 +1,5 @@
 import main
 
-def main_test():
+def test_main():
     assert main.say_hello(["Steve", "Mark", "Adam"]) == ["Hello Steve", "Hello Mark", "Hello Adam"]
     assert main.say_hello(["Micheal", "Jim", "Andy", "Stanley"]) == ["Hello Micheal", "Hello Jim", "Hello Andy", "Hello Stanley"]
